2843f477de31593c3672ae8aaa805a82125f65f1
CamilaFerreiraRodrigues <ca.rodrigues02@outlook.com>
2023-07-06 22:54:59 -0300

    ajuste no exercicio

--- java_Script/modulo3_funcoesAvançado/calculadora/assets/js/main.js
+++ java_Script/modulo3_funcoesAvançado/calculadora/assets/js/main.js
@@ -12,12 +12,52 @@ function criaCalculadora(){ // factory function
         bntClear: document.querySelector('.bnt-clear'),
         
 
+
+        pressionaEnter(){
+            this.display.addEventListener("keyup", e=>{
+                if(e.keyCode === 13){
+                    this.realizaConta();
+                }
+            })
+        },
+
+        pressionaBackspace(){ // para apagar usando teclado
+            this.display.addEventListener('keydown', e =>{
+                if (e.keyCode === 8){
+                    this.apagaUm();
+                }
+            })
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try{
+                conta = eval(conta);
+
+                if(!conta){
+                    alert('conta invalida');
+                    return;
+                }
+                this.display.value = String(conta);
+            } catch(e){
+                alert('conta invalida')
+                return;
+            }
+        },
+
         clearDisplay(){
             this.display.value = '';
         },
 
+        apagaUm(){
+            this.display.value = this.display.value.slice(0,- 1);
+        },
+
         inicia(){ //será executado quando quando chamarmos 'calculadora.inicia'
             this.cliqueBotoes();
+            this.pressionaEnter();
+            this.pressionaBackspace();
         },
 
         //metodo--- que está dentro do metodo inicia e será executadada quando ocorrer o evento de 'click'
@@ -39,7 +79,13 @@ function criaCalculadora(){ // factory function
 
                 if(el.classList.contains('btn-clear')){
                     this.clearDisplay();
-                }
+            }
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+            }
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta();
+            }
 
             });
 
